2729f6a05e896c265383a62470d379ce33f94179
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2020-08-23 12:53:46 -0500

    Now global tint settings uses its own settings specific namespace.

--- CommonsLib_APP/CHANGELOG.md
+++ CommonsLib_APP/CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.0.5] - 2020-08-23
+
+### Global tint settings now are in a separate settings namespace other than default.
+
 ## [1.0.4] - 2020-07-30
 
 ### Added Option to configure screen tint globally.
--- CommonsLib_APP/Settings/GlobalTintSettings.cs
+++ CommonsLib_APP/Settings/GlobalTintSettings.cs
@@ -7,31 +7,38 @@ namespace CommonsLib_APP.Settings
     /// </summary>
     public static class GlobalTintSettings
     {
+        private const string SettingsNamespace = "global_tint_settings";
+        
         /// <summary>
         /// Whether or not to use global custom tint while loading app screens.
         /// </summary>
         public static bool UseCustomTint
         {
-            get => Preferences.Get(nameof(UseCustomTint), false);
-            set => Preferences.Set(nameof(UseCustomTint), value);
+            get => Preferences.Get(nameof(UseCustomTint), false, SettingsNamespace);
+            set => Preferences.Set(nameof(UseCustomTint), value, SettingsNamespace);
         }
         
         public static string ToolbarBackgroundColor
         {
-            get => Preferences.Get(nameof(ToolbarBackgroundColor), "#315981");
-            set => Preferences.Set(nameof(ToolbarBackgroundColor), value);
+            get => Preferences.Get(nameof(ToolbarBackgroundColor), "#315981", SettingsNamespace);
+            set => Preferences.Set(nameof(ToolbarBackgroundColor), value, SettingsNamespace);
         }
 
         public static string ToolbarTitleColor
         {
-            get => Preferences.Get(nameof(ToolbarTitleColor), "#FFFFFF");
-            set => Preferences.Set(nameof(ToolbarTitleColor), value);
+            get => Preferences.Get(nameof(ToolbarTitleColor), "#FFFFFF", SettingsNamespace);
+            set => Preferences.Set(nameof(ToolbarTitleColor), value, SettingsNamespace);
         }
         
         public static string SystemStatusColor
         {
-            get => Preferences.Get(nameof(SystemStatusColor), "#1B3147");
-            set => Preferences.Set(nameof(SystemStatusColor), value);
+            get => Preferences.Get(nameof(SystemStatusColor), "#1B3147", SettingsNamespace);
+            set => Preferences.Set(nameof(SystemStatusColor), value, SettingsNamespace);
+        }
+
+        public static void Clear()
+        {
+            Preferences.Clear(SettingsNamespace);
         }
         
     }
